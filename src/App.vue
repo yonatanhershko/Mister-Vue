67a16ff132be7736f7d1ae2ecf9db0fe4160b554
Yonatan Hershko <yonher8@gmail.com>
2024-08-20 14:44:01 +0300

    add basic routes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,30 @@
-<script>
-</script>
-
 <template>
-  <h1>test</h1>
+  <header>
+    <nav>
+      <button v-on:click="route = 'Weather'">Weather</button>
+      <!-- <button v-on:click="route = 'SimpleCounter'">SimpleCounter</button> -->
+      <!-- <button v-on:click="route = 'PetIndex'">PetIndex</button> -->
+    </nav>
+  </header>
+
+  <Weather v-if="route === 'Weather'" />
 </template>
 
-<style scoped>
+<script>
+import Weather from "./cmps/Weather.vue";
+export default {
+  components: {
+    Weather,
+  },
+
+  data() {
+    return {
+      route: "Weather",
+    };
+  },
+};
+</script>
+
+<style lang="scss"
+
 </style>
